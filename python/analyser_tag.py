908ba2821e76768396f9cb668237ac876d748aa3
Teemo Tebest <teemo.tebest@gmail.com>
2018-02-06 17:25:22 +0200

    Added new analyser and renamed existing one.

---
+++ python/analyser_tag.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# -*- coding: UTF8 -*-
+# @See http://www.python.org/dev/peps/pep-0263/
+
+########### Python 2.7 #############
+import httplib, urllib, base64
+import json
+import sys
+import csv
+
+try:
+  file = sys.argv[1]
+  key = sys.argv[2]
+except:
+  print 'Provide file and key!!'
+  exit()
+
+headers = {
+  # Request headers
+  'Content-Type': 'application/json',
+  'Ocp-Apim-Subscription-Key': '%s' % key
+}
+params = urllib.urlencode({
+  # Request parameters
+  'visualFeatures': 'Categories',
+  'language': 'en',
+})
+
+# Initialize MongoDB
+from pymongo import MongoClient
+connection = MongoClient('localhost', 27017)
+db = connection.candidate_analyzer
+tag_db = db.tag
+
+# Clear databases.
+#tag_db.remove({})
+
+i = 0
+response = urllib.urlopen(file)
+data = json.loads(response.read())
+# reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+for person in data:
+  i = i + 1
+  body = "{'url':'%s'}" % person['14']
+  if tag_db.find({'url':person['14']}).count() == 0:
+    conn = httplib.HTTPSConnection('westeurope.api.cognitive.microsoft.com')
+    conn.request(method='POST', url='/vision/v1.0/tag', body=body, headers=headers)
+    response = conn.getresponse()
+    response_json = json.loads(response.read())
+    output = {
+      'id':person['1'],
+      'url':person['14'],
+      'response':response_json,
+      'data':person
+    }
+    tag_db.insert(output)
+    conn.close()
+  print i
