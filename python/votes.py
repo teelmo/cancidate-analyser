e4cff0f62f9dd19048f18b30b61df7786871fea7
Teemo Tebest <teemo.tebest@gmail.com>
2018-02-06 14:39:09 +0200

    Implemented and added votes.

--- python/votes.py
+++ python/votes.py
@@ -7,11 +7,16 @@ import json
 
 urls = ['http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_1.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_2.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_3.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_4.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_5.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_6.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_7.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_8.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_9.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_10.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_11.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_12.json','http://static.cdn.yle.fi/vaalit/ev2015/versions/70/candidates_13.json']
 
+output = []
 for url in urls:
   response = urllib.urlopen(url)
   data = json.loads(response.read())
   print data
-  break
+  output.append(data)
+
+
+print json.dumps(output)
+
 
 
 
