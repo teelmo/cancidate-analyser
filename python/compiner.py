029b6c3c0f74efebeec8ed2d35e8934c6c738806
Antti Saarenpaa <antti.saarenpaa@yle.fi>
2018-02-06 15:08:47 +0200

    compiner update

--- python/compiner.py
+++ python/compiner.py
@@ -2,3 +2,17 @@
 # -*- coding: UTF8 -*-
 # @See http://www.python.org/dev/peps/pep-0263/
 
+# Initialize MongoDB
+import urllib, json
+from pymongo import MongoClient
+connection = MongoClient('localhost', 27017)
+db = connection.candidate_analyzer
+person_db = db.person
+
+response = urllib.urlopen("../data/streamStarts.json")
+streams = json.loads(response.read())
+
+for stream in streams:
+    stream["id"]
+    print person_db.find({'url': "http://images.cdn.yle.fi/image/upload/13-"+stream["id"]})
+    break
