efb05e7e912ca21b173adbde740a6973180230e8
Antti Saarenpaa <antti.saarenpaa@yle.fi>
2018-02-06 14:23:50 +0200

    streamStarts script & data

--- python/streamstarts.py
+++ python/streamstarts.py
@@ -4,8 +4,19 @@
 
 import httplib, urllib, base64
 import json
+import csv
 
-url = 'http://somedata.api.yle.fi/v2/programs/streamstarts/total.json?yle_id=1-2694514&app_id=e2630534&app_key=da5935d56e4c3e1bc8d330cff2bfa672'
-response = urllib.urlopen(url)
-data = json.loads(response.read())
-print data
+results = []
+with open('../data/areena_ids.txt') as inputfile:
+    for row in csv.reader(inputfile):
+        url = 'http://somedata.api.yle.fi/v2/programs/streamstarts/total.json?yle_id=%s&app_id=e2630534&app_key=da5935d56e4c3e1bc8d330cff2bfa672' % row[0]
+        response = urllib.urlopen(url)
+        data = json.loads(response.read())
+        output = {
+                  "streamViews": data["data"][0]["streamStarts"],
+                  "id": row[0]
+        }
+        results.append(output)
+
+print json.dumps(results)
+        
